ae1902db9b0a1ffe4a807376e46304adf9f7bd22
Paul Krohn <pkrohn@daemonize.com>
2024-11-06 22:31:23 -0800

    maybe i have my csrf tokens and updates sorted out

--- src/Race.svelte
+++ src/Race.svelte
@@ -3,18 +3,71 @@
   import { onMount } from "svelte";
   let props = $props();
 
-  let race = $state(0)
+  let race = $state(0);
+  let csrf_token = $state(1);
 
   // let race; // will be populated by onMount
   onMount(async () => {
     // console.log(`going to try to get race ${id}`);
-    await fetch(`http://localhost:8000/board/race/${props.id}`)
+    await fetch(`http://localhost:8080/board/race/${props.id}`)
     .then(r => r.json())
     .then(data => {
       race = data["race"];
     });
+
+    await fetch(`http://localhost:8080/board/login`)
+    .then(r => r.json())
+    .then(data => {
+      csrf_token = data["token"];
+    });
+
   })
 
+
+  const updateScores = () => {
+    console.log("update scores called");
+    race.teams.forEach(team => {
+      console.log(`in ${race.name} team ${team.name} count is ${team.count}`);
+      updateScore(race.id, team.id, team.count);
+    })
+
+  }
+
+  const updateScore = async(race_id, team_id, count) => {
+    console.log(`updateScore called with: ${race_id}, ${team_id}, ${count}`);
+    var post_data = {
+        "id": race_id,
+        "team_id": team_id,
+        "count": count,
+      };
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Crendentials": "true",
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrf_token,
+        "X-Method": "POST",
+      },
+      credentials: 'include',
+      body: JSON.stringify(post_data)
+    };
+
+    await fetch(`http://localhost:8080/board/race/update_score`, requestOptions)
+    .then(r => r.json())
+    .then(data => {
+      race = data;
+    });
+  }
+
 </script>
-id is :{props.id}:
-{race.name}
+
+<h1>{race.name}</h1>
+
+  <form method="POST">
+    {#each race.teams as team}
+		<label>
+			{team.name}
+      <input type="number" bind:value={team.count} min="0" max={team.to} onchange={updateScores}/>
+		</label>
+    {/each}
+	</form>
